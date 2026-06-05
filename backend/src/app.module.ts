4de43d777b3fad6e211c137bb58edc5ba4cc061a
Станислав Колосов <s.kolosov@thewhite.ru>
2026-06-06 03:08:13 +1000

    add products endpoint

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, ProductsModule],
 })
 export class AppModule {}
---
+++ backend/src/products/products.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class ProductsService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    findAll() {
+        return this.prisma.product.findMany({
+            orderBy: [
+                {
+                    category: {
+                        sortOrder: 'asc',
+                    },
+                },
+                {
+                    sortOrder: 'asc',
+                },
+                {
+                    id: 'asc',
+                },
+            ],
+            include: {
+                category: true,
+            },
+        });
+    }
+}
